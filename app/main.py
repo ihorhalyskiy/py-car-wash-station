f220ac8bfa502f0b423e788834af618d5ccf49b3
Ihor Halytskiy <galytskiyihor@gmail.com>
2024-12-03 22:02:52 +0200

    Tried to fix Valentine's comments ver.1

--- app/main.py
+++ app/main.py
@@ -23,9 +23,10 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self,
-                   cars: list
-                   ) -> float:
+    def serve_cars(
+            self,
+            cars: list
+    ) -> float:
         income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -33,28 +34,32 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self,
-                                car: [Car]
-                                ) -> float:
-        washing_price = round(
-            car.comfort_class
-            * (self.clean_power - car.clean_mark)
-            * self.average_rating
-            / self.distance_from_city_center, 1
-        )
-        return washing_price
-
-    def wash_single_car(self,
-                        car: [Car]
-                        ) -> None:
+    def wash_single_car(
+            self,
+            car: [Car]
+    ) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self,
-                     rating: int
-                     ) -> float:
-        self.average_rating = round(((self.average_rating
-                                      * self.count_of_ratings) + rating)
-                                    / (self.count_of_ratings + 1), 1)
+    def calculate_washing_price(
+            self,
+            car: [Car]
+    ) -> float:
+        washing_price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+        return round(washing_price, 1)
+
+    def rate_service(
+            self,
+            rating: int
+    ) -> float:
+        self.average_rating = round(
+            ((self.average_rating
+              * self.count_of_ratings)
+             + rating) / (self.count_of_ratings + 1), 1)
         self.count_of_ratings += 1
         return self.average_rating
